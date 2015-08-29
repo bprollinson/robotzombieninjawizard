20ef7b763c7de770b445b7c60c179f270f1df0bc
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-08-29 11:26:45 -0500

    Refactoring MovementKeyListener

--- rznw/ui/MovementKeyListener.java
+++ rznw/ui/MovementKeyListener.java
@@ -9,6 +9,106 @@ import rznw.map.element.MapElement;
 
 public class MovementKeyListener implements KeyListener
 {
+    private class KeyBasedPositionChange
+    {
+        private int initialRow;
+        private int initialColumn;
+
+        private int deltaRow;
+        private int deltaColumn;
+
+        private int finalRow;
+        private int finalColumn;
+
+        public KeyBasedPositionChange(MainCharacter character, KeyEvent event)
+        {
+            MapElement mapElement = character.getMapElement();
+
+            this.initialRow = mapElement.getRow();
+            this.initialColumn = mapElement.getColumn();
+
+            this.calculateDeltas(event);
+
+            this.finalRow = this.initialRow + this.deltaRow;
+            this.finalColumn = this.initialColumn + this.deltaColumn;
+        }
+
+        private void calculateDeltas(KeyEvent event)
+        {
+            this.deltaRow = 0;
+            this.deltaColumn = 0;
+
+            switch (event.getKeyCode())
+            {
+                case KeyEvent.VK_UP:
+                case KeyEvent.VK_NUMPAD8:
+                case KeyEvent.VK_KP_UP:
+                    this.deltaRow = -1;
+                    break;
+                case KeyEvent.VK_DOWN:
+                case KeyEvent.VK_NUMPAD2:
+                case KeyEvent.VK_KP_DOWN:
+                    this.deltaRow = 1;
+                    break;
+                case KeyEvent.VK_LEFT:
+                case KeyEvent.VK_NUMPAD4:
+                case KeyEvent.VK_KP_LEFT:
+                    this.deltaColumn = -1;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                case KeyEvent.VK_NUMPAD6:
+                case KeyEvent.VK_KP_RIGHT:
+                    this.deltaColumn = 1;
+                    break;
+                case KeyEvent.VK_NUMPAD7:
+                case KeyEvent.VK_HOME:
+                    this.deltaRow = -1;
+                    this.deltaColumn = -1;
+                    break;
+                case KeyEvent.VK_NUMPAD9:
+                case KeyEvent.VK_PAGE_UP:
+                    this.deltaRow = -1;
+                    this.deltaColumn = 1;
+                    break;
+                case KeyEvent.VK_NUMPAD1:
+                case KeyEvent.VK_END:
+                    this.deltaRow = 1;
+                    this.deltaColumn = -1;
+                    break;
+                case KeyEvent.VK_NUMPAD3:
+                case KeyEvent.VK_PAGE_DOWN:
+                    this.deltaRow = 1;
+                    this.deltaColumn = 1;
+                    break;
+            }
+        }
+
+        public boolean isChange()
+        {
+            return this.deltaRow != 0 || this.deltaColumn != 0;
+        }
+
+        public int getInitialRow()
+        {
+            return this.initialRow;
+        }
+
+        public int getInitialColumn()
+        {
+            return this.initialColumn;
+        }
+
+        public int getFinalRow()
+        {
+            return this.finalRow;
+        }
+
+        public int getFinalColumn()
+        {
+            return this.finalColumn;
+        }
+    }
+
     private MainCharacter character;
     private Map map;
     private MapRenderer renderer;
@@ -26,72 +126,28 @@ public class MovementKeyListener implements KeyListener
 
     public void keyReleased(KeyEvent event)
     {
-        MapElement mapElement = this.character.getMapElement();
-
-        int deltaX = 0;
-        int deltaY = 0;
-
-        switch (event.getKeyCode())
+        KeyBasedPositionChange positionChange = new KeyBasedPositionChange(this.character, event);
+        if (!positionChange.isChange())
         {
-            case KeyEvent.VK_UP:
-            case KeyEvent.VK_NUMPAD8:
-            case KeyEvent.VK_KP_UP:
-                deltaY = -1;
-                break;
-            case KeyEvent.VK_DOWN:
-            case KeyEvent.VK_NUMPAD2:
-            case KeyEvent.VK_KP_DOWN:
-                deltaY = 1;
-                break;
-            case KeyEvent.VK_LEFT:
-            case KeyEvent.VK_NUMPAD4:
-            case KeyEvent.VK_KP_LEFT:
-                deltaX = -1;
-                break;
-            case KeyEvent.VK_RIGHT:
-            case KeyEvent.VK_NUMPAD6:
-            case KeyEvent.VK_KP_RIGHT:
-                deltaX = 1;
-                break;
-            case KeyEvent.VK_NUMPAD7:
-            case KeyEvent.VK_HOME:
-                deltaY = -1;
-                deltaX = -1;
-                break;
-            case KeyEvent.VK_NUMPAD9:
-            case KeyEvent.VK_PAGE_UP:
-                deltaY = -1;
-                deltaX = 1;
-                break;
-            case KeyEvent.VK_NUMPAD1:
-            case KeyEvent.VK_END:
-                deltaY = 1;
-                deltaX = -1;
-                break;
-            case KeyEvent.VK_NUMPAD3:
-            case KeyEvent.VK_PAGE_DOWN:
-                deltaY = 1;
-                deltaX = 1;
-                break;
-            default:
-                return;
+            return;
         }
 
-        int oldX = mapElement.getColumn();
-        int oldY = mapElement.getRow();
-        int newX = oldX + deltaX;
-        int newY = oldY + deltaY;
+        int newRow = positionChange.getFinalRow();
+        int newColumn = positionChange.getFinalColumn();
 
-        MapElement collisionTest = map.getElement(newY, newX);
+        MapElement collisionTest = map.getElement(newRow, newColumn);
         if (collisionTest != null)
         {
             return;
         }
 
-        map.setElement(oldY, oldX, null);
-        map.setElement(newY, newX, mapElement);
-        mapElement.setRow(newY);
-        mapElement.setColumn(newX);
+        map.setElement(positionChange.getInitialRow(), positionChange.getInitialColumn(), null);
+
+        MapElement mapElement = character.getMapElement();
+        map.setElement(newRow, newColumn, mapElement);
+        mapElement.setRow(newRow);
+        mapElement.setColumn(newColumn);
+
         this.renderer.render(this.map);
     }
 
