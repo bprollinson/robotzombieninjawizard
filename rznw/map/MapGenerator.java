89346ea1a98b7acebc5cfb5ada57a238d40fef72
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-08-29 08:33:35 -0500

    Refactoring MapGenerator

--- rznw/map/MapGenerator.java
+++ rznw/map/MapGenerator.java
@@ -14,6 +14,15 @@ public class MapGenerator
     {
         Map map = new Map();
 
+        this.generateTerrain(map);
+        this.placeCharacter(map, character);
+        this.generateEnemies(map, characterGenerator);
+
+        return map;
+    }
+
+    private void generateTerrain(Map map)
+    {
         for (int i = 0; i < Map.NUM_COLUMNS; i++)
         {
             map.setElement(0, i, new Wall(0, 1));
@@ -25,11 +34,17 @@ public class MapGenerator
             map.setElement(i, 0, new Wall(i, 0));
             map.setElement(i, Map.NUM_COLUMNS - 1, new Wall(i, Map.NUM_COLUMNS - 1));
         }
+    }
 
+    private void placeCharacter(Map map, GameCharacter character)
+    {
         character.generateMapElement(1, 1);
         CharacterMapElement characterMapElement = (CharacterMapElement)character.getMapElement();
         map.setElement(1, 1, characterMapElement);
+    }
 
+    private void generateEnemies(Map map, CharacterGenerator characterGenerator)
+    {
         for (int i = 10; i < 15; i++)
         {
             EnemyCharacter enemyCharacter = characterGenerator.generateEnemy();
@@ -37,7 +52,5 @@ public class MapGenerator
             EnemyMapElement enemyMapElement = (EnemyMapElement)enemyCharacter.getMapElement();
             map.setElement(i, i, enemyMapElement);
         }
-
-        return map;
     }
 }
