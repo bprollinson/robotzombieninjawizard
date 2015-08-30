adc9c6aed30ddbc20586def65181f19841d866e5
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-08-30 17:30:57 -0500

    Got rudimentary ai working so that enemies follow the main character around

--- rznw/game/enemy/EnemyCharacter.java
+++ rznw/game/enemy/EnemyCharacter.java
@@ -8,6 +8,32 @@ public abstract class EnemyCharacter extends Character
 {
     public EnemyAIBasedPositionChange getPositionChange(MainCharacter character)
     {
-        return new EnemyAIBasedPositionChange(this, -1, -1);
+        int enemyRow = this.getMapElement().getRow();
+        int enemyColumn = this.getMapElement().getColumn();
+
+        int characterRow = character.getMapElement().getRow();
+        int characterColumn = character.getMapElement().getColumn();
+
+        int deltaRow = 0;
+        if (enemyRow < characterRow)
+        {
+            deltaRow = 1;
+        }
+        if (enemyRow > characterRow)
+        {
+            deltaRow = -1;
+        }
+
+        int deltaColumn = 0;
+        if (enemyColumn < characterColumn)
+        {
+            deltaColumn = 1;
+        }
+        if (enemyColumn > characterColumn)
+        {
+            deltaColumn = -1;
+        }
+
+        return new EnemyAIBasedPositionChange(this, deltaRow, deltaColumn);
     }
 }
