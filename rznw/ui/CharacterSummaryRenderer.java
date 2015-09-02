392b74f727bf96bce1d6afb35e7a18bec63268a8
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-09-01 21:52:36 -0500

    Adding character summary renderer

---
+++ rznw/ui/CharacterSummaryRenderer.java
@@ -0,0 +1,21 @@
+package rznw.ui;
+
+import rznw.game.Character;
+
+public class CharacterSummaryRenderer
+{
+    private MainGameFrame frame;
+
+    public CharacterSummaryRenderer(MainGameFrame frame)
+    {
+        this.frame = frame;
+    }
+
+    public void render(Character character)
+    {
+        this.frame.renderDisplayString(21, 0, "HP: ");
+
+        String hp = character.getHP() + "/" + character.getMaxHP();
+        this.frame.renderDisplayString(21, 4, hp);
+    }
+}
