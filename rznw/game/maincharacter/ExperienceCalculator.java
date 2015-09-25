ae556f31f81f99651388375af68945faac970fa5
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-09-24 21:30:32 -0500

    Adding display of experience for next level on the character menu

--- rznw/game/maincharacter/ExperienceCalculator.java
+++ rznw/game/maincharacter/ExperienceCalculator.java
@@ -6,4 +6,9 @@ public class ExperienceCalculator
     {
         return (int)Math.floor(experience / 20);
     }
+
+    public static int getRequiredExperience(int level)
+    {
+        return 20 * level;
+    }
 }
--- rznw/ui/CharacterScreenRenderer.java
+++ rznw/ui/CharacterScreenRenderer.java
@@ -1,5 +1,6 @@
 package rznw.ui;
 
+import rznw.game.maincharacter.ExperienceCalculator;
 import rznw.game.maincharacter.MainCharacter;
 
 public class CharacterScreenRenderer extends MenuScreenRenderer
@@ -21,6 +22,11 @@ public class CharacterScreenRenderer extends MenuScreenRenderer
         this.frame.renderDisplayString(3, 0, "Class: " + this.character.getCharacterClass());
         this.frame.renderDisplayString(4, 0, "Level: " + this.character.getLevel());
         this.frame.renderDisplayString(5, 0, "Total experience: " + this.character.getExperience());
-        this.frame.renderDisplayString(7, 0, "HP: " + this.character.getHP() + "/" + this.character.getMaxHP());
+
+        int requiredTotalExperience = ExperienceCalculator.getRequiredExperience(this.character.getLevel() + 1);
+        int requiredAdditionalExperience = requiredTotalExperience - this.character.getExperience();
+        this.frame.renderDisplayString(6, 0, "Next level: " + requiredTotalExperience + " (" + requiredAdditionalExperience + " more)");
+
+        this.frame.renderDisplayString(8, 0, "HP: " + this.character.getHP() + "/" + this.character.getMaxHP());
     }
 }
