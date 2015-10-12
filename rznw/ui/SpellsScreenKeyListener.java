f4ebbd8c1b26f90088d78342f1fce98ca0e29766
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-10-11 22:37:58 -0500

    Adding cursor to spells menu

--- rznw/ui/SpellsScreenKeyListener.java
+++ rznw/ui/SpellsScreenKeyListener.java
@@ -8,20 +8,37 @@ public class SpellsScreenKeyListener extends StateTransitionKeyListener
 {
     private SpellsScreenRenderer spellsScreenRenderer;
     private GameWorld gameWorld;
+    private MenuState state;
 
     public SpellsScreenKeyListener(SpellsScreenRenderer spellsScreenRenderer, GameWorld gameWorld)
     {
         this.spellsScreenRenderer = spellsScreenRenderer;
         this.gameWorld = gameWorld;
+        this.state = new MenuState(15);
     }
 
     public void keyPressed(KeyEvent event)
     {
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_KP_UP:
+                this.state.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_KP_DOWN:
+                this.state.moveDown();
+                break;
+        }
+
+        this.spellsScreenRenderer.render(this.state, this.gameWorld.getMainCharacter());
     }
 
     public void enterState(int previousState)
     {
-        this.spellsScreenRenderer.render(this.gameWorld.getMainCharacter());
+        this.spellsScreenRenderer.render(this.state, this.gameWorld.getMainCharacter());
     }
 
     public void exitState(KeyEvent event)
