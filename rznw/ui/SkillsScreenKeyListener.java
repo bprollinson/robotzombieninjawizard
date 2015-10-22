95ee896e7b59cb1e78747fd3fa6dfc6240b5d1f0
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-10-21 20:24:20 -0500

    Adding cursor to the in-game skills screen

--- rznw/ui/SkillsScreenKeyListener.java
+++ rznw/ui/SkillsScreenKeyListener.java
@@ -1,5 +1,6 @@
 package rznw.ui;
 
+import rznw.game.maincharacter.MainCharacter;
 import rznw.map.GameWorld;
 
 import java.awt.event.KeyEvent;
@@ -8,20 +9,39 @@ public class SkillsScreenKeyListener extends StateTransitionKeyListener
 {
     private SkillsScreenRenderer skillsScreenRenderer;
     private GameWorld gameWorld;
+    private MenuState state;
 
     public SkillsScreenKeyListener(SkillsScreenRenderer skillsScreenRenderer, GameWorld gameWorld)
     {
         this.skillsScreenRenderer = skillsScreenRenderer;
         this.gameWorld = gameWorld;
+        this.state = new MenuState(15);
     }
 
     public void keyPressed(KeyEvent event)
     {
+        MainCharacter character = this.gameWorld.getMainCharacter();
+
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_KP_UP:
+                this.state.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_KP_DOWN:
+                this.state.moveDown();
+                break;
+        }
+
+        this.skillsScreenRenderer.render(this.state, this.gameWorld.getMainCharacter());
     }
 
     public void enterState(int previousState)
     {
-        this.skillsScreenRenderer.render(this.gameWorld.getMainCharacter());
+        this.skillsScreenRenderer.render(this.state, this.gameWorld.getMainCharacter());
     }
 
     public void exitState(KeyEvent event)
--- rznw/ui/SkillsScreenRenderer.java
+++ rznw/ui/SkillsScreenRenderer.java
@@ -4,12 +4,15 @@ import rznw.game.maincharacter.MainCharacter;
 
 public class SkillsScreenRenderer extends MenuScreenRenderer
 {
+    private static final int MENU_ENTRY_FIRST_ROW = 4;
+    private static final int MENU_ROW_HEIGHT = 1;
+
     public SkillsScreenRenderer(MainGameFrame frame)
     {
         super(frame);
     }
 
-    public void render(MainCharacter mainCharacter)
+    public void render(MenuState state, MainCharacter mainCharacter)
     {
         this.clearScreen();
         this.renderCenteredString(1, "Skills");
@@ -18,6 +21,8 @@ public class SkillsScreenRenderer extends MenuScreenRenderer
         this.renderPointGroup(mainCharacter, 1, 9);
         this.renderPointGroup(mainCharacter, 2, 15);
         this.renderPointGroup(mainCharacter, 3, 21);
+
+        this.renderCursor(state);
     }
 
     private void renderPointGroup(MainCharacter mainCharacter, int groupNumber, int startRow)
@@ -30,5 +35,13 @@ public class SkillsScreenRenderer extends MenuScreenRenderer
             int pointIndex = groupNumber * 4 + i;
             this.frame.renderDisplayString(startRow + i + 1, 2, MainCharacter.getSkillName(pointIndex) + ": " + mainCharacter.getSkillPoints(pointIndex));
         }
+    }
+
+    private void renderCursor(MenuState state)
+    {
+        int rowSpaces = 2 * (state.getEntryNumber() / 4);
+        int row = SkillsScreenRenderer.MENU_ENTRY_FIRST_ROW + state.getEntryNumber() * SkillsScreenRenderer.MENU_ROW_HEIGHT + rowSpaces;
+
+        this.frame.renderDisplayCharacter(row, 0, 'X');
     }
 }
