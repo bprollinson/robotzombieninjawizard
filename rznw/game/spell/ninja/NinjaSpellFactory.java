f8a6e8ee4d02abae9d9d339161eb9d7ac7629aea
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-11-19 22:09:59 -0600

    Implementing roundhouse strike spell

--- rznw/game/spell/ninja/NinjaSpellFactory.java
+++ rznw/game/spell/ninja/NinjaSpellFactory.java
@@ -11,6 +11,8 @@ public class NinjaSpellFactory extends SpellFactory
         {
             case 0:
                 return new StunStrikeSpell();
+            case 1:
+                return new RoundhouseStrikeSpell();
             case 5:
                 return new ShurikenStarSpell();
             default:
---
+++ rznw/game/spell/ninja/RoundhouseStrikeSpell.java
@@ -0,0 +1,87 @@
+package rznw.game.spell.ninja;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import rznw.game.enemy.EnemyCharacter;
+import rznw.game.maincharacter.MainCharacter;
+import rznw.game.spell.Spell;
+import rznw.map.GameWorld;
+import rznw.map.Map;
+import rznw.map.element.MapElement;
+
+public class RoundhouseStrikeSpell extends Spell
+{
+    public void cast(GameWorld gameWorld, int spellPoints)
+    {
+    }
+
+    public void cast(GameWorld gameWorld, int spellPoints, int direction)
+    {
+        System.out.println("Casting Roundhouse Strike");
+
+        MainCharacter character = gameWorld.getMainCharacter();
+
+        int startRow = character.getMapElement().getRow();
+        int startColumn = character.getMapElement().getColumn();
+
+        Map map = gameWorld.getMap();
+        map.setElement(startRow, startColumn, null);
+
+        int deltaRow = 0;
+        int deltaColumn = 0;
+
+        switch(direction)
+        {
+            case Spell.DIRECTION_UP:
+                deltaRow = -1;
+                break;
+            case Spell.DIRECTION_DOWN:
+                deltaRow = 1;
+                break;
+            case Spell.DIRECTION_LEFT:
+                deltaColumn = -1;
+                break;
+            case Spell.DIRECTION_RIGHT:
+                deltaColumn = 1;
+                break;
+        }
+
+        int objectRow = startRow;
+        int objectColumn = startColumn;
+
+        while (map.getElement(objectRow, objectColumn) == null)
+        {
+            objectRow += deltaRow;
+            objectColumn += deltaColumn;
+        }
+
+        int characterRow = objectRow - deltaRow;
+        int characterColumn = objectColumn - deltaColumn;
+
+        MapElement characterElement = character.getMapElement();
+        characterElement.setRow(characterRow);
+        characterElement.setColumn(characterColumn);
+        map.setElement(characterRow, characterColumn, characterElement);
+
+        Collection<EnemyCharacter> enemies = map.getEnemiesInRectangle(characterElement.getRow() - 1, characterElement.getColumn() - 1, characterElement.getRow() + 1, characterElement.getColumn() + 1);
+        for (Iterator iterator = enemies.iterator(); iterator.hasNext();)
+        {
+            EnemyCharacter enemy = (EnemyCharacter)iterator.next();
+            System.out.println("Before: " + enemy.getHP());
+            int damage = 100 + 20 * spellPoints;
+            enemy.damage(damage);
+            System.out.println("After: " + enemy.getHP());
+        }
+    }
+
+    public int getMPCost(MainCharacter character, int spellPoints)
+    {
+        return Math.max(200 - 10 * spellPoints, 1);
+    }
+
+    public boolean requiresDirectionInput()
+    {
+        return true;
+    }
+}
