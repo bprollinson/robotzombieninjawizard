8b0841e5f131c3a10fcd450edbf27ade6c56f01e
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-12-15 19:45:37 -0600

    Refactoring shortest path calculation

--- rznw/map/ShortestPathCalculator.java
+++ rznw/map/ShortestPathCalculator.java
@@ -3,6 +3,7 @@ package rznw.map;
 import rznw.map.element.Void;
 import rznw.map.element.Wall;
 import rznw.map.generator.MapPoint;
+import rznw.map.generator.direction.PathDirection;
 import rznw.map.generator.direction.PathDirectionDown;
 import rznw.map.generator.direction.PathDirectionLeft;
 import rznw.map.generator.direction.PathDirectionRight;
@@ -56,45 +57,27 @@ public class ShortestPathCalculator
             pathCache.registerPathAsUsed(paths[i]);
         }
 
+        PathDirection[] possibleDirections = new PathDirection[]{
+            new PathDirectionUp(),
+            new PathDirectionDown(),
+            new PathDirectionLeft(),
+            new PathDirectionRight()
+        };
+
         for (int i = 0; i < paths.length; i++)
         {
-            MapPath upPath = paths[i].getSubsequentPath(new PathDirectionUp());
-            MapPoint upPathPoint = upPath.getCurrentPoint();
-            if (upPathPoint.getY() >= 0 && !pathCache.pathIsUsed(upPath) && !(map.getElement(upPathPoint.getY(), upPathPoint.getX()) instanceof Wall) || upPath.getCurrentPoint().equals(endPoint))
+            for (int j = 0; j < possibleDirections.length; j++)
             {
-                if (voidWalk || !(map.getElement(upPathPoint.getY(), upPathPoint.getX()) instanceof Void))
-                {
-                    newPaths.add(upPath);
-                }
-            }
+                PathDirection possibleDirection = possibleDirections[j];
+                MapPath path = paths[i].getSubsequentPath(possibleDirection);
+                MapPoint point = path.getCurrentPoint();
 
-            MapPath downPath = paths[i].getSubsequentPath(new PathDirectionDown());
-            MapPoint downPathPoint = downPath.getCurrentPoint();
-            if (downPathPoint.getY() < Map.NUM_ROWS && !pathCache.pathIsUsed(downPath) && !(map.getElement(downPathPoint.getY(), downPathPoint.getX()) instanceof Wall) || downPath.getCurrentPoint().equals(endPoint))
-            {
-                if (voidWalk || !(map.getElement(downPathPoint.getY(), downPathPoint.getX()) instanceof Void))
+                if (point.getY() >= 0 && point.getY() < Map.NUM_ROWS && point.getX() >= 0 && point.getX() < Map.NUM_COLUMNS && !(map.getElement(point.getY(), point.getX()) instanceof Wall) || path.getCurrentPoint().equals(endPoint))
                 {
-                    newPaths.add(downPath);
-                }
-            }
-
-            MapPath leftPath = paths[i].getSubsequentPath(new PathDirectionLeft());
-            MapPoint leftPathPoint = leftPath.getCurrentPoint();
-            if (leftPathPoint.getX() >= 0 && !pathCache.pathIsUsed(leftPath) && !(map.getElement(leftPathPoint.getY(), leftPathPoint.getX()) instanceof Wall) || leftPath.getCurrentPoint().equals(endPoint))
-            {
-                if (voidWalk || !(map.getElement(leftPathPoint.getY(), leftPathPoint.getX()) instanceof Void))
-                {
-                    newPaths.add(leftPath);
-                }
-            }
-
-            MapPath rightPath = paths[i].getSubsequentPath(new PathDirectionRight());
-            MapPoint rightPathPoint = rightPath.getCurrentPoint();
-            if (rightPathPoint.getX() < Map.NUM_COLUMNS && !pathCache.pathIsUsed(rightPath) && !(map.getElement(rightPathPoint.getY(), rightPathPoint.getX()) instanceof Wall) || rightPath.getCurrentPoint().equals(endPoint))
-            {
-                if (voidWalk || !(map.getElement(rightPathPoint.getY(), rightPathPoint.getX()) instanceof Void))
-                {
-                    newPaths.add(rightPath);
+                    if (voidWalk || !(map.getElement(point.getY(), point.getX()) instanceof Void))
+                    {
+                        newPaths.add(path);
+                    }
                 }
             }
         }
