ae35e3c0071539e1dd965890ef827cef9ec2a419
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-12-17 18:15:59 -0600

    Populating base equipment menu

--- rznw/ui/EquipmentScreenKeyListener.java
+++ rznw/ui/EquipmentScreenKeyListener.java
@@ -8,20 +8,37 @@ public class EquipmentScreenKeyListener extends StateTransitionKeyListener
 {
     private EquipmentScreenRenderer equipmentScreenRenderer;
     private GameWorld gameWorld;
+    private MenuState state;
 
     public EquipmentScreenKeyListener(EquipmentScreenRenderer equipmentScreenRenderer, GameWorld gameWorld)
     {
         this.equipmentScreenRenderer = equipmentScreenRenderer;
         this.gameWorld = gameWorld;
+        this.state = new MenuState(1);
     }
 
     public void keyPressed(KeyEvent event)
     {
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_KP_UP:
+                this.state.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_KP_DOWN:
+                this.state.moveDown();
+                break;
+        }
+
+        this.equipmentScreenRenderer.render(this.state);
     }
 
     public void enterState(int previousState)
     {
-        this.equipmentScreenRenderer.render();
+        this.equipmentScreenRenderer.render(state);
     }
 
     public void exitState(KeyEvent event)
--- rznw/ui/EquipmentScreenRenderer.java
+++ rznw/ui/EquipmentScreenRenderer.java
@@ -7,9 +7,21 @@ public class EquipmentScreenRenderer extends MenuScreenRenderer
         super(frame);
     }
 
-    public void render()
+    public void render(MenuState state)
     {
         this.clearScreen();
         this.renderCenteredString(1, "Equipment");
+
+        this.renderCenteredString(4, "Weapons");
+        this.renderCenteredString(6, "Shields");
+
+        this.renderCursor(state);
+    }
+
+    private void renderCursor(MenuState state)
+    {
+        int row = 4 + 2 * state.getEntryNumber();
+
+        this.frame.renderDisplayCharacter(row, 14, 'X');
     }
 }
