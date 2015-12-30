a8c2e8c8d67f8bb38f4a224b4c6d366b705d932d
Brendan Rollinson <brendan.rollinson@gmail.com>
2015-12-29 20:25:10 -0600

    Adding some spell stats for the ninja

--- rznw/game/spell/ninja/CounterstrikeSpell.java
+++ rznw/game/spell/ninja/CounterstrikeSpell.java
@@ -18,4 +18,14 @@ public class CounterstrikeSpell extends UndirectedSpell
     {
         return Math.max(200 - 10 * spellPoints, 1);
     }
+
+    public String[] getStats(MainCharacter character, int spellPoints)
+    {
+        return new String[] {
+            "MP cost: " + this.getMPCost(character, spellPoints),
+            "Counterstrike probability: " + 10 * spellPoints + "%",
+            "Counterstrike damage: " + 10 * spellPoints,
+            "Number of turns: 1"
+        };
+    }
 }
