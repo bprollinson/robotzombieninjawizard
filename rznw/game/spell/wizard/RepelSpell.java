14914d00f9cc54fef38503d6146a75324cac5ffa
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-01-01 14:07:40 -0600

    Adding some spell stats for the wizard

--- rznw/game/spell/wizard/RepelSpell.java
+++ rznw/game/spell/wizard/RepelSpell.java
@@ -111,4 +111,16 @@ public class RepelSpell extends UndirectedSpell
     {
         return Math.max(200 - 10 * spellPoints, 1);
     }
+
+    public String[] getStats(MainCharacter character, int spellPoints)
+    {
+        int radius = 1 + (int)Math.floor(spellPoints / 4);
+        int pushDistance = 1 + (int)Math.floor(spellPoints / 4);
+
+        return new String[] {
+            "MP cost: " + this.getMPCost(character, spellPoints),
+            "Repel radius: " + radius,
+            "Repel distance: " + pushDistance
+        };
+    }
 }
