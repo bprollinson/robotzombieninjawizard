fec1a2ca9002fad29852d3075f0246e35713e3bd
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-05-31 22:05:24 -0500

    Adding save slot selection menu

--- rznw/ui/SaveScreenKeyListener.java
+++ rznw/ui/SaveScreenKeyListener.java
@@ -5,19 +5,36 @@ import java.awt.event.KeyEvent;
 public class SaveScreenKeyListener extends StateTransitionKeyListener
 {
     private SaveScreenRenderer saveScreenRenderer;
+    private MenuState state;
 
     public SaveScreenKeyListener(SaveScreenRenderer saveScreenRenderer)
     {
         this.saveScreenRenderer = saveScreenRenderer;
+        this.state = new MenuState(9);
     }
 
     public void keyPressed(KeyEvent event)
     {
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_KP_UP:
+                this.state.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_KP_DOWN:
+                this.state.moveDown();
+                break;
+        }
+
+        this.saveScreenRenderer.render(this.state);
     }
 
     public void enterState(int previousState)
     {
-        this.saveScreenRenderer.render();
+        this.saveScreenRenderer.render(this.state);
     }
 
     public void exitState(KeyEvent event)
