67d6a845a4336b1b2c2d671640affcbaac2347e8
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-06-04 05:17:57 -0500

    Adding experience to the game save

--- rznw/save/MainCharacterSaver.java
+++ rznw/save/MainCharacterSaver.java
@@ -1,5 +1,6 @@
 package rznw.save;
 
+import rznw.game.maincharacter.MainCharacter;
 import rznw.map.GameWorld;
 
 import java.io.BufferedWriter;
@@ -9,9 +10,13 @@ public class MainCharacterSaver implements ComponentSaver
 {
     public void save(GameWorld gameWorld, BufferedWriter fileWriter)
     {
+        MainCharacter mainCharacter = gameWorld.getMainCharacter();
+
         try
         {
-            fileWriter.write("" + gameWorld.getMainCharacter().getLevel());
+            fileWriter.write("" + mainCharacter.getLevel());
+            fileWriter.newLine();
+            fileWriter.write("" + mainCharacter.getExperience());
         }
         catch (IOException ioe)
         {
