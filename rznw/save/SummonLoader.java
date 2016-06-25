ab6b70070acfd2a2e3241897e32be376a88d471a
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-06-24 21:46:57 -0500

    Saving and loading number of summons

--- rznw/save/SummonLoader.java
+++ rznw/save/SummonLoader.java
@@ -8,5 +8,7 @@ public class SummonLoader extends ComponentLoader
 {
     public void load(GameWorld gameWorld, BufferedReader fileReader)
     {
+        int numSummons = this.readInteger(fileReader);
+        System.out.println("Num summons: " + numSummons);
     }
 }
--- rznw/save/SummonSaver.java
+++ rznw/save/SummonSaver.java
@@ -1,6 +1,9 @@
 package rznw.save;
 
 import rznw.map.GameWorld;
+import rznw.map.Map;
+import rznw.map.element.MapElement;
+import rznw.map.element.SummonedMinionMapElement;
 
 import java.io.BufferedWriter;
 
@@ -8,5 +11,28 @@ public class SummonSaver extends ComponentSaver
 {
     public void save(GameWorld gameWorld, BufferedWriter fileWriter)
     {
+        Map map = gameWorld.getMap();
+
+        this.writeLine(fileWriter, this.getNumSummons(map));
+    }
+
+    private int getNumSummons(Map map)
+    {
+        int numSummons = 0;
+
+        for (int row = 0; row < Map.NUM_ROWS; row++)
+        {
+            for (int column = 0; column < Map.NUM_COLUMNS; column++)
+            {
+                MapElement element = map.getElement(row, column);
+
+                if (element instanceof SummonedMinionMapElement)
+                {
+                    numSummons++;
+                }
+            }
+        }
+
+        return numSummons;
     }
 }
