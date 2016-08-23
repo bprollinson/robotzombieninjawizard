3b3dffd6200dc4c1c1be776ea38cd23550171391
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-08-23 06:40:10 -0500

    Adding more of the internals of the new game type screen input handling

--- rznw/ui/NewGameTypeScreenKeyListener.java
+++ rznw/ui/NewGameTypeScreenKeyListener.java
@@ -5,18 +5,36 @@ import java.awt.event.KeyEvent;
 public class NewGameTypeScreenKeyListener extends StateTransitionKeyListener
 {
     private NewGameTypeScreenRenderer newGameTypeScreenRenderer;
+    private MenuState state;
 
     public NewGameTypeScreenKeyListener(NewGameTypeScreenRenderer newGameTypeScreenRenderer)
     {
         this.newGameTypeScreenRenderer = newGameTypeScreenRenderer;
+        this.state = new MenuState(1);
     }
 
     public void keyPressed(KeyEvent event)
     {
+        switch (event.getKeyCode())
+        {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_KP_UP:
+                this.state.moveUp();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_KP_DOWN:
+                this.state.moveDown();
+                break;
+        }
+
+        this.newGameTypeScreenRenderer.render(this.state);
     }
 
     public void enterState(int previousState)
     {
+        this.newGameTypeScreenRenderer.render(this.state);
     }
 
     public void exitState(KeyEvent event)
@@ -25,6 +43,15 @@ public class NewGameTypeScreenKeyListener extends StateTransitionKeyListener
 
     public int getNextState(KeyEvent event)
     {
+        if (event.getKeyCode() == KeyEvent.VK_ENTER)
+        {
+            switch (this.state.getEntryNumber())
+            {
+                case 0:
+                    return DispatchKeyListener.STATE_NEW_GAME_SCREEN;
+            }
+        }
+
         return DispatchKeyListener.STATE_NEW_GAME_TYPE_SCREEN;
     }
 }
--- rznw/ui/NewGameTypeScreenRenderer.java
+++ rznw/ui/NewGameTypeScreenRenderer.java
@@ -2,8 +2,28 @@ package rznw.ui;
 
 public class NewGameTypeScreenRenderer extends MenuScreenRenderer
 {
+    private static final int MENU_ENTRY_FIRST_ROW = 3;
+    private static final int MENU_ROW_HEIGHT = 2;
+
     public NewGameTypeScreenRenderer(MainGameFrame frame)
     {
         super(frame);
+    }
+
+    public void render(MenuState state)
+    {
+        this.clearScreen();
+        this.renderCenteredString(1, "New Game Type");
+        this.renderCenteredString(3, "New Game");
+        this.renderCenteredString(5, "New Game Plus");
+
+        this.renderCursor(state);
+    }
+
+    private void renderCursor(MenuState state)
+    {
+        int row = NewGameTypeScreenRenderer.MENU_ENTRY_FIRST_ROW + state.getEntryNumber() * NewGameTypeScreenRenderer.MENU_ROW_HEIGHT;
+
+        this.frame.renderDisplayCharacter(row, 15, 'X');
     }
 }
