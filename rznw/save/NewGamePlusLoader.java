ebb4ea4c4e167c54e2206fd9652436fde79e57dd
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-09-05 16:59:29 -0500

    Refactoring common functionality out of game loader classes

--- rznw/save/NewGamePlusLoader.java
+++ rznw/save/NewGamePlusLoader.java
@@ -1,65 +1,8 @@
 package rznw.save;
 
-import rznw.map.GameWorld;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-
-public class NewGamePlusLoader
+public class NewGamePlusLoader extends BaseGameLoader
 {
-    public void load(GameWorld gameWorld, int slot) throws MissingFileException
-    {
-        File saveFile = this.getSaveFile(slot);
-        if (!saveFile.exists())
-        {
-            throw new MissingFileException(saveFile);
-        }
-
-        this.loadComponentInfo(gameWorld, saveFile);
-    }
-
-    private File getSaveFile(int slot)
-    {
-        String fileName = (slot + 1) + ".sav";
-        String filePath = "saves/" + fileName;
-
-        return new File(filePath);
-    }
-
-    private void loadComponentInfo(GameWorld gameWorld, File saveFile)
-    {
-        BufferedReader fileReader = null;
-
-        try
-        {
-            fileReader = new BufferedReader(new FileReader(saveFile));
-        }
-        catch (FileNotFoundException fnfe)
-        {
-            return;
-        }
-
-        ComponentLoader[] componentLoaders = this.getComponentLoaders();
-
-        for (int i = 0; i < componentLoaders.length; i++)
-        {
-            ComponentLoader componentLoader = componentLoaders[i];
-            componentLoader.load(gameWorld, fileReader);
-        }
-
-        try
-        {
-            fileReader.close();
-        }
-        catch (IOException ioe)
-        {
-        }
-    }
-
-    private ComponentLoader[] getComponentLoaders()
+    protected ComponentLoader[] getComponentLoaders()
     {
         return new ComponentLoader[] {
             new MainCharacterLoader(),
