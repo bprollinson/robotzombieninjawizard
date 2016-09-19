8999f293223ec28386ae154b4a2878a2263b594b
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-09-18 19:46:39 -0500

    Factoring out item kill bonus granter

---
+++ rznw/game/maincharacter/ItemKillBonusGranter.java
@@ -0,0 +1,53 @@
+package rznw.game.maincharacter;
+
+import rznw.game.enemy.EnemyCharacter;
+import rznw.game.maincharacter.inventory.InventoryFullException;
+import rznw.game.maincharacter.inventory.InventoryItemGroup;
+import rznw.game.maincharacter.inventory.Potion;
+import rznw.game.skill.Skill;
+import rznw.game.spell.robot.PowerSearchSpell;
+import rznw.utility.RandomNumberGenerator;
+
+public class ItemKillBonusGranter implements KillBonusGranterComponent
+{
+    public void grantKillBonuses(MainCharacter mainCharacter, EnemyCharacter enemyCharacter)
+    {
+        if (enemyCharacter.isDroppingItems(mainCharacter))
+        {
+            InventoryItemGroup itemGroup = new InventoryItemGroup(enemyCharacter.getItemDrop(), 1);
+
+            if (mainCharacter.getStatusEffects().powerSearchEnabled())
+            {
+                System.out.println("Upgrading item via power search");
+                itemGroup = PowerSearchSpell.getUpgradedItemGroup(itemGroup, mainCharacter.getSpellPoints(15));
+            }
+
+            try
+            {
+                mainCharacter.getInventory().addItems(itemGroup);
+            }
+            catch (InventoryFullException ife)
+            {
+                System.out.println("Inventory full");
+            }
+        }
+
+        if (mainCharacter.getSkillPoints(Skill.SKILL_POTION_FIND) > 0)
+        {
+            int probability = 5 * mainCharacter.getSkillPoints(Skill.SKILL_POTION_FIND);
+
+            if (RandomNumberGenerator.rollSucceeds(probability))
+            {
+                InventoryItemGroup itemGroup = new InventoryItemGroup(new Potion(), 1);
+                try
+                {
+                    mainCharacter.getInventory().addItems(itemGroup);
+                }
+                catch (InventoryFullException ife)
+                {
+                    System.out.println("Inventory full");
+                }
+            }
+        }
+    }
+}
--- rznw/game/maincharacter/KillBonusGranter.java
+++ rznw/game/maincharacter/KillBonusGranter.java
@@ -4,12 +4,6 @@ import rznw.game.Character;
 import rznw.game.enemy.EnemyCharacter;
 import rznw.game.maincharacter.inventory.EquipmentFullException;
 import rznw.game.maincharacter.inventory.EquipmentGroup;
-import rznw.game.maincharacter.inventory.InventoryFullException;
-import rznw.game.maincharacter.inventory.InventoryItemGroup;
-import rznw.game.maincharacter.inventory.Potion;
-import rznw.game.skill.Skill;
-import rznw.game.spell.robot.PowerSearchSpell;
-import rznw.utility.RandomNumberGenerator;
 
 public class KillBonusGranter
 {
@@ -19,7 +13,8 @@ public class KillBonusGranter
     {
         this.components = new KillBonusGranterComponent[] {
             new ExperienceKillBonusGranter(),
-            new GoldKillBonusGranter()
+            new GoldKillBonusGranter(),
+            new ItemKillBonusGranter()
         };
     }
 
@@ -39,49 +34,7 @@ public class KillBonusGranter
             component.grantKillBonuses(mainCharacter, enemyCharacter);
         }
 
-        this.grantItems(mainCharacter, enemyCharacter);
         this.grantEquipment(mainCharacter, enemyCharacter);
-    }
-
-    private void grantItems(MainCharacter mainCharacter, EnemyCharacter enemyCharacter)
-    {
-        if (enemyCharacter.isDroppingItems(mainCharacter))
-        {
-            InventoryItemGroup itemGroup = new InventoryItemGroup(enemyCharacter.getItemDrop(), 1);
-
-            if (mainCharacter.getStatusEffects().powerSearchEnabled())
-            {
-                System.out.println("Upgrading item via power search");
-                itemGroup = PowerSearchSpell.getUpgradedItemGroup(itemGroup, mainCharacter.getSpellPoints(15));
-            }
-
-            try
-            {
-                mainCharacter.getInventory().addItems(itemGroup);
-            }
-            catch (InventoryFullException ife)
-            {
-                System.out.println("Inventory full");
-            }
-        }
-
-        if (mainCharacter.getSkillPoints(Skill.SKILL_POTION_FIND) > 0)
-        {
-            int probability = 5 * mainCharacter.getSkillPoints(Skill.SKILL_POTION_FIND);
-
-            if (RandomNumberGenerator.rollSucceeds(probability))
-            {
-                InventoryItemGroup itemGroup = new InventoryItemGroup(new Potion(), 1);
-                try
-                {
-                    mainCharacter.getInventory().addItems(itemGroup);
-                }
-                catch (InventoryFullException ife)
-                {
-                    System.out.println("Inventory full");
-                }
-            }
-        }
     }
 
     private void grantEquipment(MainCharacter mainCharacter, EnemyCharacter enemyCharacter)
