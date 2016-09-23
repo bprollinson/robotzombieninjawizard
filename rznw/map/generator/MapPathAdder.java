6eca0cd4eb9d95443b551159fa22ccd8d8be3d76
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-09-22 19:57:05 -0500

    Splitting map path generator into two more managable classes

---
+++ rznw/map/generator/MapPathAdder.java
@@ -0,0 +1,91 @@
+package rznw.map.generator;
+
+import rznw.map.Map;
+import rznw.map.ShortestPathCalculator;
+import rznw.map.generator.area.MapArea;
+import rznw.map.generator.area.MapAreaPointAggregator;
+import rznw.map.generator.area.OpenAreaPadder;
+import rznw.map.generator.area.RoomRenderer;
+import rznw.map.generator.direction.PathDirection;
+import rznw.map.generator.direction.PathDirectionFactory;
+import rznw.map.generator.path.MapPath;
+import rznw.map.generator.path.PathCollection;
+import rznw.map.generator.path.PathRenderer;
+import rznw.utility.RandomNumberGenerator;
+
+import java.util.List;
+
+public class MapPathAdder
+{
+    private MapAreaPointAggregator pointAggregator;
+    private OpenAreaPadder padder;
+    private RoomRenderer roomRenderer;
+    private PathRenderer pathRenderer;
+
+    public MapPathAdder()
+    {
+        this.pointAggregator = new MapAreaPointAggregator();
+        this.padder = new OpenAreaPadder();
+        this.roomRenderer = new RoomRenderer();
+        this.pathRenderer = new PathRenderer();
+    }
+
+    public void addPath(Map map, List<MapArea> rooms, PathCollection pathCollection, int i, int j)
+    {
+        MapArea room1 = rooms.get(i);
+        MapArea room2 = rooms.get(j);
+
+        pathCollection.setDirectlyAdjacent(i, j);
+
+        PathDirection[] possibleDirectionOut = PathDirectionFactory.getFromRooms(room1, room2);
+
+        int outIndex = RandomNumberGenerator.randomInteger(0, possibleDirectionOut.length - 1);
+        PathDirection directionOut = possibleDirectionOut[outIndex];
+
+        MapPoint point1 = this.getRandomWallAdjacentPoint(room1, directionOut);
+
+        PathDirection[] possibleDirectionIn = PathDirectionFactory.getOppositeDirections(possibleDirectionOut);
+
+        int inIndex = RandomNumberGenerator.randomInteger(0, possibleDirectionIn.length - 1);
+        PathDirection directionIn = possibleDirectionIn[inIndex];
+
+        MapPoint point2 = this.getRandomWallAdjacentPoint(room2, directionIn);
+
+        MapPath path = this.calculatePath(map, rooms, point1, point2);
+
+        this.pathRenderer.renderPathOnMap(map, path);
+    }
+
+    private MapPoint getRandomWallAdjacentPoint(MapArea room, PathDirection directionFromRoom)
+    {
+        MapPoint[] wallPoints = this.pointAggregator.getWallPoints(room, directionFromRoom);
+
+        MapPoint[] possiblePoints = new MapPoint[wallPoints.length];
+
+        for (int i = 0; i < wallPoints.length; i++)
+        {
+            MapPoint wallPoint = wallPoints[i];
+            possiblePoints[i] = new MapPoint(wallPoint.getRow() + directionFromRoom.getDeltaRow(), wallPoint.getColumn() + directionFromRoom.getDeltaColumn());
+        }
+
+        int randomIndex = RandomNumberGenerator.randomInteger(0, possiblePoints.length - 1);
+        return possiblePoints[randomIndex];
+    }
+
+    private MapPath calculatePath(Map map, List<MapArea> rooms, MapPoint point1, MapPoint point2)
+    {
+        Map paddedMap = new Map(map.getLevel());
+
+        for (int i = 0; i < rooms.size(); i++)
+        {
+            MapArea room = rooms.get(i);
+            MapArea paddedRoom = this.padder.addBordersToOpenArea(room, 1);
+
+            this.roomRenderer.renderRoom(paddedMap, paddedRoom.getStartRow(), paddedRoom.getStartColumn(), paddedRoom.getEndRow(), paddedRoom.getEndColumn());
+        }
+
+        ShortestPathCalculator pathCalculator = new ShortestPathCalculator(paddedMap, true, false);
+
+        return pathCalculator.calculateShortestPath(point1, point2);
+    }
+}
--- rznw/map/generator/MapPathGenerator.java
+++ rznw/map/generator/MapPathGenerator.java
@@ -1,17 +1,8 @@
 package rznw.map.generator;
 
 import rznw.map.Map;
-import rznw.map.ShortestPathCalculator;
 import rznw.map.generator.area.MapArea;
-import rznw.map.generator.area.MapAreaPointAggregator;
-import rznw.map.generator.area.OpenAreaPadder;
-import rznw.map.generator.area.RoomRenderer;
-import rznw.map.generator.direction.PathDirection;
-import rznw.map.generator.direction.PathDirectionFactory;
-import rznw.map.generator.path.MapPath;
-import rznw.map.generator.path.MapPathCache;
 import rznw.map.generator.path.PathCollection;
-import rznw.map.generator.path.PathRenderer;
 import rznw.utility.RandomNumberGenerator;
 
 import java.util.List;
@@ -20,17 +11,11 @@ public class MapPathGenerator
 {
     private static final int RANDOM_PATH_PROBABILITY = 10;
 
-    private MapAreaPointAggregator pointAggregator;
-    private OpenAreaPadder padder;
-    private RoomRenderer roomRenderer;
-    private PathRenderer pathRenderer;
+    private MapPathAdder pathAdder;
 
     public MapPathGenerator()
     {
-        this.pointAggregator = new MapAreaPointAggregator();
-        this.padder = new OpenAreaPadder();
-        this.roomRenderer = new RoomRenderer();
-        this.pathRenderer = new PathRenderer();
+        this.pathAdder = new MapPathAdder();
     }
 
     public void generatePaths(Map map, List<MapArea> rooms)
@@ -69,7 +54,7 @@ public class MapPathGenerator
             }
         }
 
-        this.addPath(map, rooms, pathCollection, shortestI, shortestJ);
+        this.pathAdder.addPath(map, rooms, pathCollection, shortestI, shortestJ);
     }
 
     private void addAllUnsetPathsWithProbability(Map map, List<MapArea> rooms, PathCollection pathCollection)
@@ -82,69 +67,10 @@ public class MapPathGenerator
                 {
                     if (RandomNumberGenerator.rollSucceeds(MapPathGenerator.RANDOM_PATH_PROBABILITY))
                     {
-                        this.addPath(map, rooms, pathCollection, i, j);
+                        this.pathAdder.addPath(map, rooms, pathCollection, i, j);
                     }
                 }
             }
         }
-    }
-
-    private void addPath(Map map, List<MapArea> rooms, PathCollection pathCollection, int i, int j)
-    {
-        MapArea room1 = rooms.get(i);
-        MapArea room2 = rooms.get(j);
-
-        pathCollection.setDirectlyAdjacent(i, j);
-
-        PathDirection[] possibleDirectionOut = PathDirectionFactory.getFromRooms(room1, room2);
-
-        int outIndex = RandomNumberGenerator.randomInteger(0, possibleDirectionOut.length - 1);
-        PathDirection directionOut = possibleDirectionOut[outIndex];
-
-        MapPoint point1 = this.getRandomWallAdjacentPoint(room1, directionOut);
-
-        PathDirection[] possibleDirectionIn = PathDirectionFactory.getOppositeDirections(possibleDirectionOut);
-
-        int inIndex = RandomNumberGenerator.randomInteger(0, possibleDirectionIn.length - 1);
-        PathDirection directionIn = possibleDirectionIn[inIndex];
-
-        MapPoint point2 = this.getRandomWallAdjacentPoint(room2, directionIn);
-
-        MapPath path = this.calculatePath(map, rooms, point1, point2);
-
-        this.pathRenderer.renderPathOnMap(map, path);
-    }
-
-    private MapPoint getRandomWallAdjacentPoint(MapArea room, PathDirection directionFromRoom)
-    {
-        MapPoint[] wallPoints = this.pointAggregator.getWallPoints(room, directionFromRoom);
-
-        MapPoint[] possiblePoints = new MapPoint[wallPoints.length];
-
-        for (int i = 0; i < wallPoints.length; i++)
-        {
-            MapPoint wallPoint = wallPoints[i];
-            possiblePoints[i] = new MapPoint(wallPoint.getRow() + directionFromRoom.getDeltaRow(), wallPoint.getColumn() + directionFromRoom.getDeltaColumn());
-        }
-
-        int randomIndex = RandomNumberGenerator.randomInteger(0, possiblePoints.length - 1);
-        return possiblePoints[randomIndex];
-    }
-
-    private MapPath calculatePath(Map map, List<MapArea> rooms, MapPoint point1, MapPoint point2)
-    {
-        Map paddedMap = new Map(map.getLevel());
-
-        for (int i = 0; i < rooms.size(); i++)
-        {
-            MapArea room = rooms.get(i);
-            MapArea paddedRoom = this.padder.addBordersToOpenArea(room, 1);
-
-            this.roomRenderer.renderRoom(paddedMap, paddedRoom.getStartRow(), paddedRoom.getStartColumn(), paddedRoom.getEndRow(), paddedRoom.getEndColumn());
-        }
-
-        ShortestPathCalculator pathCalculator = new ShortestPathCalculator(paddedMap, true, false);
-
-        return pathCalculator.calculateShortestPath(point1, point2);
     }
 }
