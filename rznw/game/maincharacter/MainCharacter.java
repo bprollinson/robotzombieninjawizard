f319852941a7061981044f5a7fb71559a414cc53
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-09-24 06:40:09 -0500

    Factoring damage response out of main character class

--- rznw/game/maincharacter/MainCharacter.java
+++ rznw/game/maincharacter/MainCharacter.java
@@ -18,7 +18,6 @@ import rznw.game.skill.SkillFactory;
 import rznw.game.spell.SpellFactory;
 import rznw.game.stat.Stat;
 import rznw.game.stat.StatFactory;
-import rznw.game.spell.ninja.SmokeBombSpell;
 import rznw.map.GameWorld;
 import rznw.utility.RandomNumberGenerator;
 
@@ -396,95 +395,7 @@ public abstract class MainCharacter extends Character
 
         this.HP -= damage;
 
-        if (this.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_MANA_SUCK) > 0)
-        {
-            int MPFromDamage = (int)Math.floor(5.0 / 100.0 * this.getSkillPoints(Skill.SKILL_MANA_SUCK) * damage);
-            if (MPFromDamage > 0)
-            {
-                System.out.println("Healing MP from damage: " + MPFromDamage);
-                this.healMP(MPFromDamage);
-            }
-        }
-
-        if (damageSource == null)
-        {
-            return damage;
-        }
-
-        if (this.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_COUNTERSTRIKE) && damageSource.isEnemy())
-        {
-            System.out.println("Checking counterstrike");
-
-            int counterstrikeProbability = 10 * this.getSpellPoints(13);
-            if (RandomNumberGenerator.rollSucceeds(counterstrikeProbability))
-            {
-                System.out.println("Damaging with counterstrike");
-
-                int counterstrikeDamage = 10 * this.getSpellPoints(13);
-                System.out.println("Enemy hp before: " + damageSource.getHP());
-                damageSource.damage(counterstrikeDamage, this, gameWorld, Character.DAMAGE_SOURCE_MAGICAL);
-                System.out.println("Enemy hp after: " + damageSource.getHP());
-            }
-
-            this.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_COUNTERSTRIKE, false);
-        }
-
-        if (this.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_THORN_SKIN) > 0 && damageSource.isEnemy())
-        {
-            System.out.println("Attacking back with thorn skin");
-
-            int thornSkinDamage = 5 * this.getSpellPoints(2);
-            System.out.println("Enemy hp before: " + damageSource.getHP());
-            damageSource.damage(thornSkinDamage, this, gameWorld, Character.DAMAGE_SOURCE_MAGICAL);
-            System.out.println("Enemy hp after: " + damageSource.getHP());
-        }
-
-        if (this.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_POISON_SKIN) > 0 && damageSource.isEnemy())
-        {
-            System.out.println("Poisoning with poison skin");
-
-            damageSource.getStatusEffects().poison();
-        }
-
-        if (this.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_DEATH_STRIKE) && damageSource.isEnemy())
-        {
-            System.out.println("Checking death strike");
-
-            int deathStrikeProbability = 5 * this.getSpellPoints(15);
-            if (RandomNumberGenerator.rollSucceeds(deathStrikeProbability))
-            {
-                System.out.println("Killing with death strike");
-                damageSource.setHP(0);
-            }
-
-            this.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_DEATH_STRIKE, false);
-        }
-
-        if (this.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_SMOKE_BOMB) && damageSource.isEnemy())
-        {
-            System.out.println("Checking smoke bomb");
-
-            int smokeBombProbability = 5 * this.getSpellPoints(12);
-            if (RandomNumberGenerator.rollSucceeds(smokeBombProbability))
-            {
-                System.out.println("Escaping with smoke bomb");
-                SmokeBombSpell.escape(gameWorld);
-            }
-
-            this.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_SMOKE_BOMB, false);
-        }
-
-        if (this.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_BARBED_SKIN) > 0 && damageSource.isEnemy())
-        {
-            System.out.println("Checking barbed skin");
-
-            int barbedSkinProbability = 5 * this.getSpellPoints(3);
-            if (RandomNumberGenerator.rollSucceeds(barbedSkinProbability))
-            {
-                System.out.println("Stunning with barbed skin");
-                damageSource.getStatusEffects().setStatusEffectTurns(StatusEffects.EFFECT_FROZEN, 2);
-            }
-        }
+        new MainCharacterDamageResponder().respondToDamage(gameWorld, this, damageSource, damage);
 
         return damage;
     }
---
+++ rznw/game/maincharacter/MainCharacterDamageResponder.java
@@ -0,0 +1,104 @@
+package rznw.game.maincharacter;
+
+import rznw.game.Character;
+import rznw.game.StatusEffects;
+import rznw.game.skill.Skill;
+import rznw.game.spell.ninja.SmokeBombSpell;
+import rznw.map.GameWorld;
+import rznw.utility.RandomNumberGenerator;
+
+public class MainCharacterDamageResponder
+{
+    public void respondToDamage(GameWorld gameWorld, MainCharacter mainCharacter, Character damageSource, int damage)
+    {
+        if (mainCharacter.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_MANA_SUCK) > 0)
+        {
+            int MPFromDamage = (int)Math.floor(5.0 / 100.0 * mainCharacter.getSkillPoints(Skill.SKILL_MANA_SUCK) * damage);
+            if (MPFromDamage > 0)
+            {
+                System.out.println("Healing MP from damage: " + MPFromDamage);
+                mainCharacter.healMP(MPFromDamage);
+            }
+        }
+
+        if (damageSource == null)
+        {
+            return;
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_COUNTERSTRIKE) && damageSource.isEnemy())
+        {
+            System.out.println("Checking counterstrike");
+
+            int counterstrikeProbability = 10 * mainCharacter.getSpellPoints(13);
+            if (RandomNumberGenerator.rollSucceeds(counterstrikeProbability))
+            {
+                System.out.println("Damaging with counterstrike");
+
+                int counterstrikeDamage = 10 * mainCharacter.getSpellPoints(13);
+                System.out.println("Enemy hp before: " + damageSource.getHP());
+                damageSource.damage(counterstrikeDamage, mainCharacter, gameWorld, Character.DAMAGE_SOURCE_MAGICAL);
+                System.out.println("Enemy hp after: " + damageSource.getHP());
+            }
+
+            mainCharacter.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_COUNTERSTRIKE, false);
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_THORN_SKIN) > 0 && damageSource.isEnemy())
+        {
+            System.out.println("Attacking back with thorn skin");
+
+            int thornSkinDamage = 5 * mainCharacter.getSpellPoints(2);
+            System.out.println("Enemy hp before: " + damageSource.getHP());
+            damageSource.damage(thornSkinDamage, mainCharacter, gameWorld, Character.DAMAGE_SOURCE_MAGICAL);
+            System.out.println("Enemy hp after: " + damageSource.getHP());
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_POISON_SKIN) > 0 && damageSource.isEnemy())
+        {
+            System.out.println("Poisoning with poison skin");
+
+            damageSource.getStatusEffects().poison();
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_DEATH_STRIKE) && damageSource.isEnemy())
+        {
+            System.out.println("Checking death strike");
+
+            int deathStrikeProbability = 5 * mainCharacter.getSpellPoints(15);
+            if (RandomNumberGenerator.rollSucceeds(deathStrikeProbability))
+            {
+                System.out.println("Killing with death strike");
+                damageSource.setHP(0);
+            }
+
+            mainCharacter.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_DEATH_STRIKE, false);
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffect(StatusEffects.EFFECT_SMOKE_BOMB) && damageSource.isEnemy())
+        {
+            System.out.println("Checking smoke bomb");
+
+            int smokeBombProbability = 5 * mainCharacter.getSpellPoints(12);
+            if (RandomNumberGenerator.rollSucceeds(smokeBombProbability))
+            {
+                System.out.println("Escaping with smoke bomb");
+                SmokeBombSpell.escape(gameWorld);
+            }
+
+            mainCharacter.getStatusEffects().setStatusEffect(StatusEffects.EFFECT_SMOKE_BOMB, false);
+        }
+
+        if (mainCharacter.getStatusEffects().getStatusEffectTurns(StatusEffects.EFFECT_BARBED_SKIN) > 0 && damageSource.isEnemy())
+        {
+            System.out.println("Checking barbed skin");
+
+            int barbedSkinProbability = 5 * mainCharacter.getSpellPoints(3);
+            if (RandomNumberGenerator.rollSucceeds(barbedSkinProbability))
+            {
+                System.out.println("Stunning with barbed skin");
+                damageSource.getStatusEffects().setStatusEffectTurns(StatusEffects.EFFECT_FROZEN, 2);
+            }
+        }
+    }
+}
