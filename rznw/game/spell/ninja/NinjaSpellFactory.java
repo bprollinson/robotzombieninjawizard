12202ef6c2f21525e772dbcfab577746839e9f11
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-10-04 06:36:44 -0500

    Adding ninja spell constants

--- rznw/game/spell/ninja/NinjaSpellFactory.java
+++ rznw/game/spell/ninja/NinjaSpellFactory.java
@@ -5,41 +5,58 @@ import rznw.game.spell.SpellFactory;
 
 public class NinjaSpellFactory extends SpellFactory
 {
+    private static final int SPELL_STUN_STRIKE = 0;
+    private static final int SPELL_ROUNDHOUSE_STRIKE = 1;
+    private static final int SPELL_POISON_STRIKE = 2;
+    private static final int SPELL_ARMOR_BREAK = 3;
+    private static final int SPELL_PIN_STRIKE = 4;
+    private static final int SPELL_SHURIKEN_STAR = 5;
+    private static final int SPELL_SMOKE_CLUSTER = 6;
+    private static final int SPELL_STUN_BOMB = 7;
+    private static final int SPELL_STEAL_GOLD = 8;
+    private static final int SPELL_STEAL_ITEM = 9;
+    private static final int SPELL_STEAL_EQUIPMENT = 10;
+    private static final int SPELL_STEAL_EXPERIENCE = 11;
+    private static final int SPELL_SMOKE_BOMB = 12;
+    private static final int SPELL_COUNTERSTRIKE = 13;
+    private static final int SPELL_REVERSE_PAIN = 14;
+    private static final int SPELL_DEATH_STRIKE = 15;
+
     public Spell getSpell(int spellIndex)
     {
         switch (spellIndex)
         {
-            case 0:
+            case NinjaSpellFactory.SPELL_STUN_STRIKE:
                 return new StunStrikeSpell();
-            case 1:
+            case NinjaSpellFactory.SPELL_ROUNDHOUSE_STRIKE:
                 return new RoundhouseStrikeSpell();
-            case 2:
+            case NinjaSpellFactory.SPELL_POISON_STRIKE:
                 return new PoisonStrikeSpell();
-            case 3:
+            case NinjaSpellFactory.SPELL_ARMOR_BREAK:
                 return new ArmorBreakSpell();
-            case 4:
+            case NinjaSpellFactory.SPELL_PIN_STRIKE:
                 return new PinStrikeSpell();
-            case 5:
+            case NinjaSpellFactory.SPELL_SHURIKEN_STAR:
                 return new ShurikenStarSpell();
-            case 6:
+            case NinjaSpellFactory.SPELL_SMOKE_CLUSTER:
                 return new SmokeClusterSpell();
-            case 7:
+            case NinjaSpellFactory.SPELL_STUN_BOMB:
                 return new StunBombSpell();
-            case 8:
+            case NinjaSpellFactory.SPELL_STEAL_GOLD:
                 return new StealGoldSpell();
-            case 9:
+            case NinjaSpellFactory.SPELL_STEAL_ITEM:
                 return new StealItemSpell();
-            case 10:
+            case NinjaSpellFactory.SPELL_STEAL_EQUIPMENT:
                 return new StealEquipmentSpell();
-            case 11:
+            case NinjaSpellFactory.SPELL_STEAL_EXPERIENCE:
                 return new StealExperienceSpell();
-            case 12:
+            case NinjaSpellFactory.SPELL_SMOKE_BOMB:
                 return new SmokeBombSpell();
-            case 13:
+            case NinjaSpellFactory.SPELL_COUNTERSTRIKE:
                 return new CounterstrikeSpell();
-            case 14:
+            case NinjaSpellFactory.SPELL_REVERSE_PAIN:
                 return new ReversePainSpell();
-            case 15:
+            case NinjaSpellFactory.SPELL_DEATH_STRIKE:
                 return new DeathStrikeSpell();
             default:
                 return null;
