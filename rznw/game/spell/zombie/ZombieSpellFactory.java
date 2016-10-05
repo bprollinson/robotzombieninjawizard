8be53f16b10069a5fb187f478fadd469e43dd824
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-10-04 21:55:41 -0500

    Adding zombie spell constants

--- rznw/game/spell/zombie/ZombieSpellFactory.java
+++ rznw/game/spell/zombie/ZombieSpellFactory.java
@@ -5,41 +5,58 @@ import rznw.game.spell.SpellFactory;
 
 public class ZombieSpellFactory extends SpellFactory
 {
+    private static final int SPELL_POISON_SKIN = 0;
+    private static final int SPELL_RESIST_DAMAGE = 1;
+    private static final int SPELL_THORN_SKIN = 2;
+    private static final int SPELL_BARBED_SKIN = 3;
+    private static final int SPELL_FEED_FLESH = 4;
+    private static final int SPELL_FEED_BRAIN = 5;
+    private static final int SPELL_FEED_PAST = 6;
+    private static final int SPELL_FEED_MIND = 7;
+    private static final int SPELL_LOCUST_SWARM = 8;
+    private static final int SPELL_POISON_CLOUD = 9;
+    private static final int SPELL_INFECTIOUS_RAGE = 10;
+    private static final int SPELL_BLOTCH = 11;
+    private static final int SPELL_SUMMON_ZOMBIE = 12;
+    private static final int SPELL_INFER_ZOMBIE = 13;
+    private static final int SPELL_MULTIPLY_ZOMBIES = 14;
+    private static final int SPELL_EXPLODE_ZOMBIES = 15;
+
     public Spell getSpell(int spellIndex)
     {
         switch (spellIndex)
         {
-            case 0:
+            case ZombieSpellFactory.SPELL_POISON_SKIN:
                 return new PoisonSkinSpell();
-            case 1:
+            case ZombieSpellFactory.SPELL_RESIST_DAMAGE:
                 return new ResistDamageSpell();
-            case 2:
+            case ZombieSpellFactory.SPELL_THORN_SKIN:
                 return new ThornSkinSpell();
-            case 3:
+            case ZombieSpellFactory.SPELL_BARBED_SKIN:
                 return new BarbedSkinSpell();
-            case 4:
+            case ZombieSpellFactory.SPELL_FEED_FLESH:
                 return new FeedFleshSpell();
-            case 5:
+            case ZombieSpellFactory.SPELL_FEED_BRAIN:
                 return new FeedBrainSpell();
-            case 6:
+            case ZombieSpellFactory.SPELL_FEED_PAST:
                 return new FeedPastSpell();
-            case 7:
+            case ZombieSpellFactory.SPELL_FEED_MIND:
                 return new FeedMindSpell();
-            case 8:
+            case ZombieSpellFactory.SPELL_LOCUST_SWARM:
                 return new LocustSwarmSpell();
-            case 9:
+            case ZombieSpellFactory.SPELL_POISON_CLOUD:
                 return new PoisonCloudSpell();
-            case 10:
+            case ZombieSpellFactory.SPELL_INFECTIOUS_RAGE:
                 return new InfectiousRageSpell();
-            case 11:
+            case ZombieSpellFactory.SPELL_BLOTCH:
                 return new BlotchSpell();
-            case 12:
+            case ZombieSpellFactory.SPELL_SUMMON_ZOMBIE:
                 return new SummonZombieSpell();
-            case 13:
+            case ZombieSpellFactory.SPELL_INFER_ZOMBIE:
                 return new InferZombieSpell();
-            case 14:
+            case ZombieSpellFactory.SPELL_MULTIPLY_ZOMBIES:
                 return new MultiplyZombiesSpell();
-            case 15:
+            case ZombieSpellFactory.SPELL_EXPLODE_ZOMBIES:
                 return new ExplodeZombiesSpell();
             default:
                 return null;
