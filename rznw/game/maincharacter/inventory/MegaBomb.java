6b19fffaf905f8f7abe46f84699c5238ca95b40f
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-11-09 17:54:48 -0600

    Fixing import order in MegaBomb

--- rznw/game/maincharacter/inventory/MegaBomb.java
+++ rznw/game/maincharacter/inventory/MegaBomb.java
@@ -1,14 +1,14 @@
 package rznw.game.maincharacter.inventory;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 import rznw.game.Character;
 import rznw.game.enemy.EnemyCharacter;
 import rznw.game.maincharacter.MainCharacter;
 import rznw.map.GameWorld;
 import rznw.map.Map;
 import rznw.map.element.MapElement;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 public class MegaBomb extends InventoryItem
 {
