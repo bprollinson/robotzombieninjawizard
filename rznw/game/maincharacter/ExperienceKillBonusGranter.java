d542a166401e3167004257a4a7dfeb670faa61ef
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-12-17 08:29:46 -0600

    Cleaning up some feed past functionality

--- rznw/game/maincharacter/ExperienceKillBonusGranter.java
+++ rznw/game/maincharacter/ExperienceKillBonusGranter.java
@@ -7,13 +7,23 @@ public class ExperienceKillBonusGranter implements KillBonusGranterComponent
 {
     public void grantKillBonuses(MainCharacter mainCharacter, EnemyCharacter enemyCharacter)
     {
+        int experience = enemyCharacter.getExperienceReward();
+        this.grantExperience(mainCharacter, experience, false);
+    }
+
+    public void grantExperience(MainCharacter mainCharacter, int experience, boolean logExperience)
+    {
         int oldLevel = mainCharacter.getExperience().getLevel();
 
-        int experience = enemyCharacter.getExperienceReward();
         int maxExperience = ExperienceCalculator.getMaxExperience();
         experience = Math.min(experience, maxExperience - mainCharacter.getExperience().getExperience());
         mainCharacter.getExperience().grantExperience(experience);
         int newLevel = ExperienceCalculator.getLevel(mainCharacter.getExperience().getExperience());
+
+        if (logExperience)
+        {
+            LogRendererFactory.instance().log("Gained " + experience + " bonus experience.");
+        }
 
         if (newLevel > oldLevel)
         {
--- rznw/game/spell/zombie/FeedPastSpell.java
+++ rznw/game/spell/zombie/FeedPastSpell.java
@@ -2,6 +2,7 @@ package rznw.game.spell.zombie;
 
 import rznw.game.Character;
 import rznw.game.enemy.EnemyCharacter;
+import rznw.game.maincharacter.ExperienceKillBonusGranter;
 import rznw.game.maincharacter.MainCharacter;
 import rznw.game.spell.DirectedSpell;
 import rznw.map.GameWorld;
@@ -48,8 +49,7 @@ public class FeedPastSpell extends DirectedSpell
             damage = enemy.damage(damage, character, gameWorld, Character.DAMAGE_SOURCE_MAGICAL);
             LogRendererFactory.instance().log("Dealt " + damage + " damage to " + enemy.getLogName() + ".");
 
-            gameWorld.getMainCharacter().getExperience().grantExperience(experience);
-            LogRendererFactory.instance().log("Gained " + experience + " bonus experience.");
+            new ExperienceKillBonusGranter().grantExperience(gameWorld.getMainCharacter(), experience, true);
         }
     }
 
