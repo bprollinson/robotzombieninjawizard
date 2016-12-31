e4daa6a22920f425952d19a0361cf51bd9dc4f13
Brendan Rollinson <brendan.rollinson@gmail.com>
2016-12-31 08:11:40 -0600

    Reducing grid spacing and increasing version number

--- rznw/Version.java
+++ rznw/Version.java
@@ -2,7 +2,7 @@ package rznw;
 
 public class Version
 {
-    private static final String VERSION_STRING = "1.0.0";
+    private static final String VERSION_STRING = "1.0.1";
 
     public static String getVersionString()
     {
