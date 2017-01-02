ecb592f35d315168d41045cab58720638c7905b0
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-01-01 19:03:20 -0600

    Increasing version number

--- rznw/Version.java
+++ rznw/Version.java
@@ -2,7 +2,7 @@ package rznw;
 
 public class Version
 {
-    private static final String VERSION_STRING = "1.0.1";
+    private static final String VERSION_STRING = "1.0.2";
 
     public static String getVersionString()
     {
